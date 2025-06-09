5a27fac66dca54ca05e381bd7ae2c1fb6988b91e
Cristian Beraha <cristian@cloudops.works>
2025-06-09 13:23:13 -0300

    Add support for `resource_refs` and `principal_refs` in IAM roles and policies
    Updated logic to process `resource_refs` and `principal_refs`, enabling dynamic ARN resolution for resources and principals. Enhanced flexibility for defining IAM policies and roles while maintaining backward compatibility.

--- variables-iam.tf
+++ variables-iam.tf
@@ -4,17 +4,68 @@
 #            Distributed Under Apache v2.0 License
 #
 
+## Role definition Structure YAML
+# roles:
+#   - name_prefix: string
+#     description: string # (optional)
+#     path: string # (optional)
+#     assume_roles:
+#       - actions: list(string)
+#         type: string
+#         principals: list(string)
+#         principal_refs: list(string) # (optional)
+#         conditions: # (optional)
+#           - test: string
+#             values: list(string)
+#             variable: string
+#     managed_policies: list(string)
+#     policy_refs: list(string)
+#     inline_policies:
+#       - name: string
+#         statements:
+#           - sid: string
+#             effect: string
+#             actions: list(string)
+#             resources: list(string)
+#             resource_refs: list(string)
+#             conditions:
+#               - test: string
+#                 values: list(string)
+#                 variable: string
 variable "roles" {
   description = "A list of IAM roles to create"
   type        = any
   default     = []
 }
 
+
+## Policy definition Structure YAML
+#
+# policies:
+#   - name_prefix: string
+#     description: string              # (optional)
+#     statements:
+#       - sid: string
+#         effect: string
+#         actions: list(string)
+#         resources: list(string)
+#         resource_refs: list(string) # (optional)
+#         conditions:                 # (optional)
+#           - test: string
+#             values: list(string)
+#             variable: string
 variable "policies" {
   description = "A list of IAM policies to create"
   type        = any
   default     = []
 }
+
+## Service Linked Roles definition Structure YAML
+#
+# service_linked_roles:
+#   - service: string
+#     description: string # (optional)
+#     suffix: string      # (optional)
 
 variable "service_linked_roles" {
   description = "A list of service roles to create"
